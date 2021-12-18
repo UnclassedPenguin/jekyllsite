42f32df1a66d32b534c99d19c7f88e0aaf8a97a6
UnclassedPenguin <tvanden@live.com>
2021-12-18 00:23:07 -0700

    change colors

--- assets/js/getRandomColor.js
+++ assets/js/getRandomColor.js
@@ -1,6 +1,3 @@
-function getRandomColor() {
-	var r = document.querySelector(':root');
-
 	const colors = ["#81EF9E",
 			"#9E81EF",
 			"#EF9E81",
@@ -10,6 +7,33 @@ function getRandomColor() {
 			"#81D2EF",
 			"#CB98EB",
 			"#98B8EB"];
+
+function getRandomColor() {
+  return colors[Math.floor(Math.random() * colors.length)];
+}
+
+function setRandomLinkColor() {
+  Array.from(document.getElementsByTagName("a")).forEach((e) => {
+    e.style.color = getRandomColor();
+  });
+}
+
+setRandomLinkColor();
+
+
+/*
+function getRandomColor() {
+	var r = document.querySelector(':root');
+	const colors = ["#81EF9E",
+			"#9E81EF",
+			"#EF9E81",
+			"#EF81D2",
+			"#D581EF",
+			"#81EFD5",
+			"#81D2EF",
+			"#CB98EB",
+			"#98B8EB"];
+
 	var length = colors.length
 	var rand = Math.floor((Math.random() * length) + 0);
 	r.style.setProperty('--rand', colors[rand])
@@ -18,3 +42,4 @@ function getRandomColor() {
 }
 
 getRandomColor();
+*/
