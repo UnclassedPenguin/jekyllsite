3ae07fc93e28384d7f260ee4167f8f691860e0b0
UnclassedPenguin <tvanden@live.com>
2021-12-17 19:17:24 -0700

    fix formatting

--- assets/js/getRandomColor.js
+++ assets/js/getRandomColor.js
@@ -2,14 +2,14 @@ function getRandomColor() {
 	var r = document.querySelector(':root');
 
 	const colors = ["#81EF9E",
-					"#9E81EF",
-					"#EF9E81",
-					"#EF81D2",
-					"#D581EF",
-					"#81EFD5",
-					"#81D2EF",
-					"#CB98EB",
-					"#98B8EB"];
+			"#9E81EF",
+			"#EF9E81",
+			"#EF81D2",
+			"#D581EF",
+			"#81EFD5",
+			"#81D2EF",
+			"#CB98EB",
+			"#98B8EB"];
 	var length = colors.length
 	var rand = Math.floor((Math.random() * length) + 0);
 	r.style.setProperty('--rand', colors[rand])
