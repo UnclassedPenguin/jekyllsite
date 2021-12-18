144d218190494915818ca58fc3a175b521147af4
UnclassedPenguin <tvanden@live.com>
2021-12-17 19:14:28 -0700

    add more colors to getRandomColor.js

--- assets/js/getRandomColor.js
+++ assets/js/getRandomColor.js
@@ -1,8 +1,15 @@
 function getRandomColor() {
 	var r = document.querySelector(':root');
 
-	const colors = ["#9e81ef", "#ef9e81", "#81ef9e",
-					"#98B8EB"];
+	const colors = ["#81EF9E",
+"#9E81EF",
+"#EF9E81",
+"#EF81D2",
+"#D581EF",
+"#81EFD5",
+"#81D2EF",
+"#CB98EB",
+"#98B8EB"];
 	var length = colors.length
 	var rand = Math.floor((Math.random() * length) + 0);
 	r.style.setProperty('--rand', colors[rand])
